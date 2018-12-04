7b598b568205651cb415fd347024abb99004f37e
Damon T Quire <dtquir01@louisville.edu>
2018-12-04 11:06:45 -0500

    3 app functionality

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ using PodioCore.Utils.ItemFields;
 using PodioCore.Items;
 using BrickBridge;
 using PodioCore.Models;
-using PodioCore.Comments;
 using System.Collections;
 using Task = System.Threading.Tasks.Task;
 using File = Google.Apis.Drive.v3.Data.File;
@@ -24,7 +23,19 @@ using BrickBridge.Lambda.VilCap;
 
 namespace vilcapCopyFileToGoogleDrive
 {
-    public class CopyFileToGoogleDrive : saasafrasLambdaBaseFunction.Function
+    public class RoutedPodioEvent
+    {
+        public PodioEvent podioEvent { get; set; }
+        public string clientId { get; set; }
+        public string version { get; set; }
+        public string solutionId { get; set; }
+        public string environmentId { get; set; }
+    };
+
+
+
+
+    public class CopyFileToGoogleDrive
     {
         private int GetfieldId(string key)
         {
@@ -36,43 +47,67 @@ namespace vilcapCopyFileToGoogleDrive
         int fieldId;
 
         static string[] Scopes = { DriveService.Scope.DriveReadonly };
-        static string ApplicationName = "VilCap";
+        static string ApplicationName = "BrickBridgeVilCap";
         static LambdaMemoryStore memoryStore = new LambdaMemoryStore();
-
-        public override async System.Threading.Tasks.Task InnerHandler(RoutedPodioEvent e, ILambdaContext context)
+        string cId;
+        string eId;
+        string sId;
+        string v;
+        string baseUrl;
+        string apiKey;
+        public async Task<int> GetID(string key)
         {
-            context.Logger.LogLine($"Podio proxy url: {Config.PODIO_PROXY_URL}, bbc service url: {Config.BBC_SERVICE_URL}, bbc service api key: {Config.BBC_SERVICE_API_KEY}");
-            System.Environment.SetEnvironmentVariable("PODIO_PROXY_URL", Config.PODIO_PROXY_URL);
-            System.Environment.SetEnvironmentVariable("BBC_SERVICE_URL", Config.BBC_SERVICE_URL);
-            System.Environment.SetEnvironmentVariable("BBC_SERVICE_API_KEY", Config.BBC_SERVICE_API_KEY);
+            BbcServiceClient bbc = new BbcServiceClient(baseUrl, apiKey);//TODO
+            var x = await bbc.GetKey(cId,eId,sId,v, key);
+            return int.Parse(x);
+        }
+        public async System.Threading.Tasks.Task FunctionHandler(RoutedPodioEvent e, ILambdaContext context)
+        {
+            cId = e.clientId;
+            eId = e.environmentId;
+            sId = e.solutionId;
+            v = e.version;
+            baseUrl = System.Environment.GetEnvironmentVariable("BBC_SERVICE_URL");
+            apiKey = System.Environment.GetEnvironmentVariable("BBC_SERVICE_API_KEY");
             Console.WriteLine("LETS GO");
             context.Logger.LogLine($"e.version={e.version}");
             context.Logger.LogLine($"e.clientId={e.clientId}");
             context.Logger.LogLine($"e.clientId={e.clientId}");
-            context.Logger.LogLine($"e.currentEnvironment.environmentId={e.currentEnvironment.environmentId}");
-            var factory = new AuditedPodioClientFactory(e.appId, e.version, e.clientId, e.currentEnvironment.environmentId);
-            var podio = factory.ForClient(e.clientId, e.currentEnvironment.environmentId);
-
-            vilcapSpaces = e.currentEnvironment.deployments.First(a => a.appId == "vilcap").deployedSpaces;
+            context.Logger.LogLine($"e.currentEnvironment.environmentId={e.environmentId}");
+            context.Logger.LogLine($"{Newtonsoft.Json.JsonConvert.SerializeObject(e.podioEvent)}");
+            //started here
+            var factory = new AuditedPodioClientFactory(e.solutionId, e.version, e.clientId, e.environmentId);
+            var podio = factory.ForClient(e.clientId, e.environmentId);
+            
+            
+            //vilcapSpaces = e.currentEnvironment.deployments.First(a => a.appId == "vilcap").deployedSpaces;
 
             fieldId = 0;
+            Item currentItem = await podio.GetItem(int.Parse(e.podioEvent.item_id));
+            ////These are stored in AWS Lambda
+            //string client_id = System.Environment.GetEnvironmentVariable("GOOGLE_API_CLIENT_ID");
+            //string client_secret = System.Environment.GetEnvironmentVariable("GOOGLE_API_CLIENT_SECRET");
 
-            //These are stored in AWS Lambda
-            string client_id = System.Environment.GetEnvironmentVariable("GOOGLE_API_CLIENT_ID");
-            string client_secret = System.Environment.GetEnvironmentVariable("GOOGLE_API_CLIENT_SECRET");
+            //UserCredential credential;
+            //var secrets = new ClientSecrets
+            //{
+            //    ClientId = client_id,
+            //    ClientSecret = client_secret
+            //};
+            //credential = GoogleWebAuthorizationBroker.AuthorizeAsync(secrets, Scopes, "toolkit@vilcap.com", System.Threading.CancellationToken.None, memoryStore).Result;
 
-            UserCredential credential;
-            var secrets = new ClientSecrets
-            {
-                ClientId = client_id,
-                ClientSecret = client_secret
-            };
-            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(secrets, Scopes, "toolkit@vilcap.com", System.Threading.CancellationToken.None, memoryStore).Result;
-
+            //// Create Drive API service.
+            //var service = new DriveService(new BaseClientService.Initializer()
+            //{
+            //    HttpClientInitializer = credential,
+            //    ApplicationName = ApplicationName,
+            //});
+            string serviceAcccount = System.Environment.GetEnvironmentVariable("GOOGLE_SERVICE_ACCOUNT");
+            var cred = GoogleCredential.FromJson(serviceAcccount).CreateScoped(Scopes).UnderlyingCredential;
             // Create Drive API service.
             var service = new DriveService(new BaseClientService.Initializer()
             {
-                HttpClientInitializer = credential,
+                HttpClientInitializer = cred,
                 ApplicationName = ApplicationName,
             });
 
@@ -82,18 +117,35 @@ namespace vilcapCopyFileToGoogleDrive
             //var X_ID = e.podioEvent.external_id;
             //var CLIENT_SECRET = "JqvyW2a3SdhRzD7BUkYvJ66UI6nNkuVQfRZZXAcZGi5JksFVTiCtzkTIUek2CR3h"; //ex
             //var CLIENT_WS_ID = e.clientId;
-            var cloneFolderId = e.currentItem.App.Name;
+            var cloneFolderId = currentItem.App.Name;
             //IAccessTokenProvider CLIENT_ID = null;
 
             ItemService itemService = new ItemService(podio);
             fieldId = GetfieldId("VC Toolkit Template|Task List|Parent ID");//add in the field ID's key for "Parent ID"
-            var parentId = Convert.ToInt32(e.currentItem.Field<TextItemField>(fieldId).Value);
+            var parentId = Convert.ToInt32(currentItem.Field<TextItemField>(fieldId).Value);
             Item parentItem = await itemService.GetItem(parentId);
-            Item clone = new Item { ItemId = e.currentItem.ItemId };
+            Item clone = new Item { ItemId = currentItem.ItemId };
 
             //TODO: Add in multi app functionality when deployed spaces dict is ready to go
-            var PARENT_EMBED_FIELD = "link";
-            var CHILD_EMBED_FIELD = "linked-files";
+            string PARENT_EMBED_FIELD="";
+            int CHILD_EMBED_FIELD=0;
+            switch(currentItem.App.Name)
+            {
+                case "Task List":
+                    CHILD_EMBED_FIELD= await GetID("VC Toolkit Template|Task List|Linked Files");
+                    PARENT_EMBED_FIELD = "link";
+                    break;
+
+                case "Workshop Modules":
+                    CHILD_EMBED_FIELD = await GetID("VC Toolkit Template|Workshop Modules|Link to Material");
+                    PARENT_EMBED_FIELD = "gdrive-file-name";
+                    break;
+                case "Surveys":
+                    CHILD_EMBED_FIELD = await GetID("VC Toolkit Template|Surveys|Link to Survey");
+                    PARENT_EMBED_FIELD = "gdrive-survey";
+                    break;
+                    
+            }
             EmbedItemField parentEmbedField = parentItem.Field<EmbedItemField>(PARENT_EMBED_FIELD);
             EmbedItemField cloneEmbedField = clone.Field<EmbedItemField>(CHILD_EMBED_FIELD);
             IEnumerable<Embed> parentEmbeds = parentEmbedField.Embeds;
@@ -125,7 +177,7 @@ namespace vilcapCopyFileToGoogleDrive
 
             Task.Run(() =>
             {   // Todo Implement: { "type": "anyone", "role": "writer" }
-                Google.Apis.Drive.v3.Data.Permission permission = null;
+                            Google.Apis.Drive.v3.Data.Permission permission = null;
                 new PermissionsResource.CreateRequest(ds, permission, clone.Id).Execute();
             });
 
@@ -141,7 +193,7 @@ namespace vilcapCopyFileToGoogleDrive
             FilesResource.ListRequest listReq = ds.Files.List();
             listReq.Q = "name='" + title + "'"; // Todo: format         
             return listReq.Execute().Files[0].Id;
-        }
 
+        }
     }
 }
