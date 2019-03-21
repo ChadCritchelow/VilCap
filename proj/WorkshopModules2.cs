140381ab49cc863ffb57d86f95536ab515789cb3
GemCarry <ref.johng@gmail.com>
2019-03-21 11:03:44 -0400

    dep task relationship

--- proj/WorkshopModules2.cs
+++ proj/WorkshopModules2.cs
@@ -339,7 +339,7 @@ namespace newVilcapCopyFileToGoogleDrive
                         var newTaskId = await podio.CreateItem(cloneT, tasklistAppId, true); //child Task List appId
                         cloneT.ItemId = newTaskId;
                         childTasks.ItemId = newTaskId;
-                        context.Logger.LogLine($"newTaskId {newTaskId} - cloned itemId {cloneT.ItemId} - Items {childTasks.Items}");
+                        context.Logger.LogLine($"newTaskId {newTaskId} - cloned itemId {cloneT.ItemId} - Items {childTasks.Items.ToString()}");
                         context.Logger.LogLine($"Created Dependent Task"); 
                     }
                     catch (PodioUnavailableException ex)
