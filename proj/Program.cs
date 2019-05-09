3a8be081545499e111f2c6c10e67db2d27a03c21
GemCarry <ref.johng@gmail.com>
2019-05-09 15:39:00 -0400

    moving testedu to v3

--- proj/Program.cs
+++ proj/Program.cs
@@ -80,7 +80,7 @@ namespace newVilcapCopyFileToGoogleDrive
                 {"usfintech2019" ,"US Fintech 2019" },
                 {"useducation2019", "US Education 2019" },
                 {"wepowerenvironment" ,"WePower" },
-                {"middlegameventures", "Middlegame Ventures" },
+                {"middlegameventures", "Middlegame Ventures" }//,
                 //{"testuseducation2019", "TEST - US Education 2019" }
             };
 
