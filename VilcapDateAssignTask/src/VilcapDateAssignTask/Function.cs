b5f4fa577bb9535862ef417359f8acbc1bf39b4f
GemCarry <ref.johng@gmail.com>
2019-05-13 11:04:34 -0400

    handler

--- VilcapDateAssignTask/src/VilcapDateAssignTask/Function.cs
+++ VilcapDateAssignTask/src/VilcapDateAssignTask/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
 using PodioCore;
 using PodioCore.Models;
 using PodioCore.Utils.ItemFields;
@@ -23,80 +24,82 @@ namespace VilcapDateAssignTask
     {
 
 		static LambdaMemoryStore memoryStore = new LambdaMemoryStore();
-		public async System.Threading.Tasks.Task FunctionHandler(RoutedPodioEvent e, ILambdaContext context)
-		{
-			var factory = new AuditedPodioClientFactory(e.solutionId, e.version, e.clientId, e.environmentId);
-			var podio = factory.ForClient(e.clientId, e.environmentId);
-			Item check = await podio.GetItem(Convert.ToInt32(e.podioEvent.item_id));
-			SaasafrasClient saasafrasClient = new SaasafrasClient(System.Environment.GetEnvironmentVariable("BBC_SERVICE_URL"), System.Environment.GetEnvironmentVariable("BBC_SERVICE_API_KEY"));
-			var dictChild = await saasafrasClient.GetDictionary(e.clientId, e.environmentId, e.solutionId, e.version);
-			var dictMaster = await saasafrasClient.GetDictionary("vcadministration", "vcadministration", "vilcap", "0.0");
-			string lockValue;
-			GetIds ids = new GetIds(dictChild, dictMaster, e.environmentId);
-			//Make sure to implement by checking to see if Deploy Curriculum has just changed
-			//Deploy Curriculum field
-			string functionName="VilcapDateAssignTask";
-			lockValue = await saasafrasClient.LockFunction(functionName, check.ItemId.ToString());
-			try
-			{
-				if (string.IsNullOrEmpty(lockValue))
-				{
-					context.Logger.LogLine($"Failed to acquire lock for {functionName} and id {check.ItemId}");
-					return;
-				}
+		//public async System.Threading.Tasks.Task FunctionHandler(RoutedPodioEvent e, ILambdaContext context)
+        public async System.Threading.Tasks.Task FunctionHandler(Amazon.Lambda.SNSEvents.SNSEvent sns, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Got SNSEvent: {sns.ToString()}");
+			//var factory = new AuditedPodioClientFactory(e.solutionId, e.version, e.clientId, e.environmentId);
+			//var podio = factory.ForClient(e.clientId, e.environmentId);
+			//Item check = await podio.GetItem(Convert.ToInt32(e.podioEvent.item_id));
+			//SaasafrasClient saasafrasClient = new SaasafrasClient(System.Environment.GetEnvironmentVariable("BBC_SERVICE_URL"), System.Environment.GetEnvironmentVariable("BBC_SERVICE_API_KEY"));
+			//var dictChild = await saasafrasClient.GetDictionary(e.clientId, e.environmentId, e.solutionId, e.version);
+			//var dictMaster = await saasafrasClient.GetDictionary("vcadministration", "vcadministration", "vilcap", "0.0");
+			//string lockValue;
+			//GetIds ids = new GetIds(dictChild, dictMaster, e.environmentId);
+			////Make sure to implement by checking to see if Deploy Curriculum has just changed
+			////Deploy Curriculum field
+			//string functionName="VilcapDateAssignTask";
+			//lockValue = await saasafrasClient.LockFunction(functionName, check.ItemId.ToString());
+			//try
+			//{
+			//	if (string.IsNullOrEmpty(lockValue))
+			//	{
+			//		context.Logger.LogLine($"Failed to acquire lock for {functionName} and id {check.ItemId}");
+			//		return;
+			//	}
 
-				TaskService taskServ = new TaskService(podio);
+			//	TaskService taskServ = new TaskService(podio);
 
-				var fieldIdToSearch = ids.GetFieldId("Task List|Date");
-				var filterValue = DateTime.Now.AddDays(7);
-				var filter = new Dictionary<int, object>
-							{
-								{ fieldIdToSearch, filterValue }
-							};
-				FilterOptions newOptions = new FilterOptions
-				{
-					Filters = filter,
-					Offset = 500
-				};
-				context.Logger.LogLine("Checking for duplicates");
+			//	var fieldIdToSearch = ids.GetFieldId("Task List|Date");
+			//	var filterValue = DateTime.Now.AddDays(7);
+			//	var filter = new Dictionary<int, object>
+			//				{
+			//					{ fieldIdToSearch, filterValue }
+			//				};
+			//	FilterOptions newOptions = new FilterOptions
+			//	{
+			//		Filters = filter,
+			//		Offset = 500
+			//	};
+			//	context.Logger.LogLine("Checking for duplicates");
 
-				var filteredItems = await podio.FilterItems(ids.GetFieldId("Task List"), newOptions);
+			//	var filteredItems = await podio.FilterItems(ids.GetFieldId("Task List"), newOptions);
 
-				var furtherFilteredItems = from f in filteredItems.Items
-										   where
-										   f.Field<CategoryItemField>(ids.GetFieldId("Task List|Completetion")).Options.Any()
-										   &&
-										   f.Field<CategoryItemField>(ids.GetFieldId("Task List|Completetion")).Options.First().Text != "Complete"
-										   select f;
+			//	var furtherFilteredItems = from f in filteredItems.Items
+			//							   where
+			//							   f.Field<CategoryItemField>(ids.GetFieldId("Task List|Completetion")).Options.Any()
+			//							   &&
+			//							   f.Field<CategoryItemField>(ids.GetFieldId("Task List|Completetion")).Options.First().Text != "Complete"
+			//							   select f;
 
-				foreach (var item in furtherFilteredItems)
-				{
-					var responsibleMember = item.Field<ContactItemField>(ids.GetFieldId("Task List|Responsible Member"));
-					var title = item.Field<TextItemField>(ids.GetFieldId("Task List|Title"));
-					var date = item.Field<DateItemField>(ids.GetFieldId("Task List|Date"));
-					var description = item.Field<TextItemField>(ids.GetFieldId("Task List|Description"));
-					TaskCreateUpdateRequest t = new TaskCreateUpdateRequest();
-					t.Description = title.Value;
-					List<int> cIds = new List<int>();
-					foreach (var contact in responsibleMember.Contacts)
-					{
-						cIds.Add(Convert.ToInt32(contact.UserId));
-					}
-					t.SetResponsible(cIds);
-					t.DueDate = date.Start;
-					t.Text = description.Value;
-					var task = await taskServ.CreateTask(t);
-					await taskServ.AssignTask(int.Parse(task.First().TaskId));//neccessary?
-				}
-			}
-			catch(Exception ex)
-			{
-				throw ex;
-			}
-			finally
-			{
-				await saasafrasClient.UnlockFunction(functionName, check.ItemId.ToString(), lockValue);
-			}
+			//	foreach (var item in furtherFilteredItems)
+			//	{
+			//		var responsibleMember = item.Field<ContactItemField>(ids.GetFieldId("Task List|Responsible Member"));
+			//		var title = item.Field<TextItemField>(ids.GetFieldId("Task List|Title"));
+			//		var date = item.Field<DateItemField>(ids.GetFieldId("Task List|Date"));
+			//		var description = item.Field<TextItemField>(ids.GetFieldId("Task List|Description"));
+			//		TaskCreateUpdateRequest t = new TaskCreateUpdateRequest();
+			//		t.Description = title.Value;
+			//		List<int> cIds = new List<int>();
+			//		foreach (var contact in responsibleMember.Contacts)
+			//		{
+			//			cIds.Add(Convert.ToInt32(contact.UserId));
+			//		}
+			//		t.SetResponsible(cIds);
+			//		t.DueDate = date.Start;
+			//		t.Text = description.Value;
+			//		var task = await taskServ.CreateTask(t);
+			//		await taskServ.AssignTask(int.Parse(task.First().TaskId));//neccessary?
+			//	}
+			//}
+			//catch(Exception ex)
+			//{
+			//	throw ex;
+			//}
+			//finally
+			//{
+			//	await saasafrasClient.UnlockFunction(functionName, check.ItemId.ToString(), lockValue);
+			//}
 		}
 	}
 }
