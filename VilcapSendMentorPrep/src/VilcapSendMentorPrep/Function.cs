48ed02821c70bd0cc63a2cb532658efb19ca399f
John Gries <john@brickbridgeconsulting.com>
2019-05-24 14:11:59 -0400

    auth

--- VilcapSendMentorPrep/src/VilcapSendMentorPrep/Function.cs
+++ VilcapSendMentorPrep/src/VilcapSendMentorPrep/Function.cs
@@ -34,7 +34,7 @@ namespace VilcapSendMentorPrep
             //
             try
             {
-                google.SendEmail(gmail, "me", "SUB", "BOD", "me", "john@brickbridgeconsulting.com", "VilCap", "dev");
+                google.SendEmail(gmail, "me", "SUB", "BOD", "john@brickbridgeconsulting", "john@brickbridgeconsulting.com", "VilCap", "dev");
                 context.Logger.LogLine("--- Email sent.");
             }
             //
